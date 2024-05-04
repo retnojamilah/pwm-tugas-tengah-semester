958ef628ebb33be4495d7a83bdb4be0ed8e05072
Retno <retnodharsosuharjp@gmail.com>
2024-05-04 14:17:23 +0700

    membuat website cafe kost karunia

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-// toggle class active
+// toggle class active untuk humberger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu diklik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+};
+
+//modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  // agar setiap tbutton eye/detail bisa keluar semua halaman detail
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+//klil tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+//klik diluar modal
+//const itemDetailModal = document.querySelector("#item-detail-modal");
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
 };
